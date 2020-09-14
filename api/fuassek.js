6744e4d46491bb6187a8b8f81a1c9f523db081d8
luctst <lucas.tostee@gmail.com>
2020-09-14 18:26:06 +0200

    End

--- api/fuassek.js
+++ api/fuassek.js
@@ -2,42 +2,55 @@ require("dotenv").config();
 const {MongoClient} = require('mongodb');
 
 module.exports = async function (req, res) {
-    if (req.method !== 'POST') {
+    if (req.method !== 'POST' || !req.method !== 'GET') {
         return res.status(405).json({
             error: true,
             message: 'Bad message'
         });
     }
 
-    const bodyProps = Object.keys(req.body);
-
-    if (
-        !bodyProps.includes('content') ||
-        !bodyProps.includes('author')
-    ) {
-        return res.status(402).json({
-            error: true,
-            message: 'You must have content and author.'
-        });
-    }
+    let mongo;
+    let messagesCollections;
 
     try {
-        const mongo = await MongoClient.connect(process.env.DB_ACCESS, {
+        mongo = await MongoClient.connect(process.env.DB_ACCESS, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
 
-        const messagesCollection = await mongo.db().collection('messages');
-        await messagesCollection.insertOne({ ...req.body });
-        const messagesList = await messagesCollection.find().toArray();
-
-        return res.status(200).json({
-            messages: [ ...messagesList ]
-        });
+        messagesCollections = mongo.db().collection('messages');
     } catch (error) {
         return res.status(500).json({
             error: true,
             message: error.message
         })
     }
+
+    if (req.method === 'POST') {
+        const bodyProps = Object.keys(req.body);
+    
+        if (
+            !bodyProps.includes('content') ||
+            !bodyProps.includes('author')
+        ) {
+            return res.status(402).json({
+                error: true,
+                message: 'You must have content and author.'
+            });
+        }
+
+        await messagesCollection.insertOne({ ...req.body });
+
+        return res.status(200).json({
+            message: 'Message added'
+        });
+    }
+
+    if (req.method === 'GET') {
+        const messagesList = await messagesCollections.find().toArray();
+
+        return res.status(200).json({
+            messages: [ ...messagesList ]
+        });
+    } 
 };
