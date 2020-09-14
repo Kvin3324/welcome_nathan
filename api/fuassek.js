05769d244fb77bfb8c8ae5c7315d1ef1bfb0b58e
luctst <lucas.tostee@gmail.com>
2020-09-14 18:19:33 +0200

    Update

--- api/fuassek.js
+++ api/fuassek.js
@@ -1,5 +1,43 @@
+require("dotenv").config();
+const {MongoClient} = require('mongodb');
+
 module.exports = async function (req, res) {
-    return res.status(200).json({
-        name: 'Kévin joya best war eu'
-    });
+    if (req.method !== 'POST') {
+        return res.status(405).json({
+            error: true,
+            message: 'Bad message'
+        });
+    }
+
+    const bodyProps = Object.keys(req.body);
+
+    if (
+        !bodyProps.includes('content') ||
+        !bodyProps.includes('author')
+    ) {
+        return res.status(402).json({
+            error: true,
+            message: 'You must have content and author.'
+        });
+    }
+
+    try {
+        const mongo = await MongoClient.connect(process.env.DB_ACCESS, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        const messagesCollection = await mongo.db().collection('messages');
+        await messagesCollection.insertOne({ ...req.body });
+        const messagesList = await messagesCollection.find().toArray();
+
+        return res.status(200).json({
+            messages: [ ...messagesList ]
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: error.message
+        })
+    }
 };
